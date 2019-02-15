fede254d4f772769b90311130b147505ee3a749d
jojo <yoyosource@nidido.de>
2019-02-15 22:21:08 +0100

    Chat API

---
+++ src/main/java/de/wk/betacore/appearance/Chat.java
@@ -0,0 +1,34 @@
+package de.wk.betacore.appearance;
+
+import org.bukkit.entity.Player;
+
+public class Chat {
+
+    public static void sendMessage(Player p, String message, String prefix, Boolean allowColor, Boolean allowSpecial) {
+        send(p, message, prefix, allowColor, allowSpecial);
+    }
+
+    public static void sendMessage(Player p, String message, String prefix) {
+        send(p, message, prefix, false, false);
+    }
+
+    public static void sendMessage(Player p, String message) {
+        send(p, message, "", false, false);
+    }
+
+    private static void send(Player p, String message, String prefix, Boolean allowColor, Boolean allowSpecial) {
+        prefix = prefix.replaceAll("\\(World\\)", p.getWorld().getName()).replaceAll("\\(Name\\)", p.getName()).replaceAll("&?", "");
+        prefix = Color.ConvertColor(Color.ConvertSpecial(prefix));
+        if (allowColor  == true && allowSpecial == true) {
+            message = Color.ConvertColor(Color.ConvertSpecial(message));
+        } else if (allowColor  == false && allowSpecial == true) {
+            message = Color.ConvertSpecial(message);
+        } else if (allowColor  == true && allowSpecial == false) {
+            message = Color.ConvertColor(message);
+        } else {
+
+        }
+        p.sendMessage(prefix + " §r" + message);
+    }
+
+}
