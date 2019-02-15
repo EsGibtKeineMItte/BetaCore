918e5a50fff2e84cd2e55caa56948ccfdda2d27d
leonhard <Quaste17HD@t-online.de>
2019-02-15 23:03:32 +0100

    DataSetter added

--- src/main/java/de/wk/betacore/listener/Spigot/DataSetter.java
+++ src/main/java/de/wk/betacore/listener/Spigot/DataSetter.java
@@ -12,7 +12,7 @@ public class DataSetter implements Listener {
     ConfigManager cm = new ConfigManager();
 
     @EventHandler
-    public void onLogin(PlayerLoginEvent e){
+    public void onLogin(PlayerLoginEvent e) {
         //Rank
         if (cm.getPlayerData().getString(e.getPlayer().getUniqueId().toString() + ".rank") == null) {
             cm.getPlayerData().setString(e.getPlayer().getUniqueId().toString() + ".rank", "USER");
@@ -24,16 +24,20 @@ public class DataSetter implements Listener {
         if (cm.getPlayerData().getString(e.getPlayer().getUniqueId().toString() + ".name") == null) {
             cm.getPlayerData().setString(e.getPlayer().getUniqueId().toString() + ".name", e.getPlayer().getName());
         }
-        if(cm.getPlayerData().getInt(e.getPlayer().getUniqueId() + ".wsrank") == 0){
+        if (!(cm.getPlayerData().getBoolean(e.getPlayer().getUniqueId().toString() + ".muted"))) {
+            cm.getPlayerData().setBoolean(e.getPlayer().getUniqueId().toString() + "muted", false);
+        }
+
+        if (cm.getPlayerData().getInt(e.getPlayer().getUniqueId() + ".wsrank") == 0) {
             cm.getPlayerData().setInt(e.getPlayer().getUniqueId() + ".wsrank", 900);
         }
     }
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e){
+    public void onJoin(PlayerJoinEvent e) {
         LocalDate heute = LocalDate.now();
-        if(e.getPlayer().hasPlayedBefore() || cm.getPlayerData().getString(e.getPlayer().getUniqueId().toString() + ".firstjoin") == null){
-            cm.getPlayerData().setString(e.getPlayer().getUniqueId().toString() + ".firstjoin",heute.toString());
+        if (e.getPlayer().hasPlayedBefore() || cm.getPlayerData().getString(e.getPlayer().getUniqueId().toString() + ".firstjoin") == null) {
+            cm.getPlayerData().setString(e.getPlayer().getUniqueId().toString() + ".firstjoin", heute.toString());
             return;
         }
 
