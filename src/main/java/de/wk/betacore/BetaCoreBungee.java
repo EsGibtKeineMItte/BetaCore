0b6a02f1ad48b3369e873e65706a759a5cb2d7e7
leonhard <Quaste17HD@t-online.de>
2019-02-15 21:21:54 +0100

    Initial commit

---
+++ src/main/java/de/wk/betacore/BetaCoreBungee.java
@@ -0,0 +1,5 @@
+package de.wk.betacore;
+
+public class BetaCoreBungee {
+    //
+}
---
+++ src/main/java/de/wk/betacore/Betacore.java
@@ -0,0 +1,17 @@
+package de.wk.betacore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Betacore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
