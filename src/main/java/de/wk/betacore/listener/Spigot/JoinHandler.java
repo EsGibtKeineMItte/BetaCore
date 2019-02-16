e099323cc96442ec92adb78c941cabd336334796
jojo <yoyosource@nidido.de>
2019-02-16 12:23:26 +0100

    ScoreBoard Update

--- src/main/java/de/wk/betacore/listener/Spigot/JoinHandler.java
+++ src/main/java/de/wk/betacore/listener/Spigot/JoinHandler.java
@@ -3,12 +3,14 @@ package de.wk.betacore.listener.Spigot;
 import de.wk.betacore.appearance.Scoreboard;
 import de.wk.betacore.appearance.Tablist;
 import de.wk.betacore.util.ConfigManager;
+import de.wk.betacore.util.ranksystem.RankSystem;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinHandler implements Listener {
     ConfigManager cm = new ConfigManager();
+    RankSystem rankSystem = new RankSystem();
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
@@ -17,7 +19,7 @@ public class JoinHandler implements Listener {
     }
 
     private void scoreboard(PlayerJoinEvent e) {
-        String[] st = new String[10];
+        String[] st = new String[12];
         st[0] = "&6";
         st[1] = "&6> &7Joins";
         st[2] = "&6> &7(Joins)";
@@ -26,6 +28,8 @@ public class JoinHandler implements Listener {
         st[5] = "&7";
         st[6] = "&6> &7Money";
         st[7] = "&6> &7" + cm.getPlayerData().getInt(e.getPlayer().getUniqueId().toString() + ".money");
+        st[6] = "&6> &Rank";
+        st[7] = "&6> " + rankSystem.getRank(e.getPlayer().getUniqueId()).getColor() + rankSystem.getRank(e.getPlayer().getUniqueId()).getName();
         st[8] = "&8";
         st[9] = "&6TheWarking.de";
         Scoreboard.updateScoreboard("&aTheWarKing", st, e.getPlayer());
