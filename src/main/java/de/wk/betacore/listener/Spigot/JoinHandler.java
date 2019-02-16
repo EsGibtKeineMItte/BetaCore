d6c223c3d777808397b4514a8ed1704513a46548
jojo <yoyosource@nidido.de>
2019-02-16 12:42:47 +0100

    Tablist Player

--- src/main/java/de/wk/betacore/listener/Spigot/JoinHandler.java
+++ src/main/java/de/wk/betacore/listener/Spigot/JoinHandler.java
@@ -1,8 +1,10 @@
 package de.wk.betacore.listener.Spigot;
 
+import de.wk.betacore.appearance.Color;
 import de.wk.betacore.appearance.Scoreboard;
 import de.wk.betacore.appearance.Tablist;
 import de.wk.betacore.util.ConfigManager;
+import de.wk.betacore.util.ranksystem.Rank;
 import de.wk.betacore.util.ranksystem.RankSystem;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,13 +18,14 @@ public class JoinHandler implements Listener {
     public void onJoin(PlayerJoinEvent e) {
         scoreboard(e);
         tablist(e);
+        playerTablist(e);
     }
 
     private void scoreboard(PlayerJoinEvent e) {
         String[] st = new String[12];
         st[0] = "&6";
         st[1] = "&6> &7Money";
-        st[2] = "&6> &e&7" + cm.getPlayerData().getInt(e.getPlayer().getUniqueId().toString() + ".money");
+        st[2] = "&6> &e" + cm.getPlayerData().getInt(e.getPlayer().getUniqueId().toString() + ".money");
         st[3] = "&6> &7Rank";
         st[4] = "&6> " + rankSystem.getRank(e.getPlayer().getUniqueId()).getColor() + rankSystem.getRank(e.getPlayer().getUniqueId()).getName();
         st[5] = "&7";
@@ -37,6 +40,15 @@ public class JoinHandler implements Listener {
 
     private void tablist(PlayerJoinEvent e) {
         Tablist.Tablist("&aTheWarKing(n)&7dein WarShip Server", "&7Viel Spaß auf dem Server(n)&e(Name)&7!", e.getPlayer());
+    }
+
+    private void playerTablist(PlayerJoinEvent e) {
+        e.getPlayer().setDisplayName(e.getPlayer().getName());
+        if (rankSystem.getRank(e.getPlayer().getUniqueId()).equals(Rank.USER)) {
+            e.getPlayer().setDisplayName(Color.ConvertColor(rankSystem.getRank(e.getPlayer().getUniqueId()).getColor() + e.getPlayer().getName()));
+        } else {
+            e.getPlayer().setDisplayName(Color.ConvertColor(rankSystem.getRank(e.getPlayer().getUniqueId()).getColor() + rankSystem.getRank(e.getPlayer().getUniqueId()).getName() + " " + e.getPlayer().getName()));
+        }
     }
 
 }
