c58f1489f9dd82d4b3cfd00036c0614c16494696
leonhard <Quaste17HD@t-online.de>
2019-02-16 12:13:09 +0100

    RankFix

--- src/main/java/de/wk/betacore/util/ranksystem/Rank.java
+++ src/main/java/de/wk/betacore/util/ranksystem/Rank.java
@@ -2,14 +2,14 @@ package de.wk.betacore.util.ranksystem;
 
 public enum Rank {
 
-    ADMIN("Admin", "&4"),
-    DEV("Dev", "&3"),
-    MOD("Mod", "&c"),
-    SUPPORTER("Supp", "&1"),
-    ARCHI("Builder", "&a"),
-    YOU_TUBER("YT", "&5"),
-    PREMIUM("Premium", "&6"),
-    USER("User", "&7");
+    ADMIN("Admin", "§4"),
+    DEV("Dev", "§3"),
+    MOD("Mod", "§c"),
+    SUPPORTER("Supp", "§1"),
+    ARCHI("Builder", "§a"),
+    YOU_TUBER("YT", "§5"),
+    PREMIUM("Premium", "§6"),
+    USER("User", "§7");
 
 
     private String name;
