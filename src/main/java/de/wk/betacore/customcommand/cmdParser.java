efdcd7f93ef3198118e7f445364eeab31fe701e5
maxsp <max.spaeth01@gmail.com>
2019-02-17 13:30:17 +0100

    Custom Command

--- src/main/java/de/wk/betacore/customcommand/cmdParser.java
+++ src/main/java/de/wk/betacore/customcommand/cmdParser.java
@@ -1,5 +1,15 @@
 package de.wk.betacore.customcommand;
 
+import org.bukkit.entity.Player;
+
 public class cmdParser {
+
+    public cmdParser(){
+
+    }
+
+    public static void Pars(Player p, String cmd){
+
+    }
 
 }
