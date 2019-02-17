8ea19580d3117caba839efec6c8e361ee798e9b4
maxsp <max.spaeth01@gmail.com>
2019-02-17 13:23:49 +0100

    Custom Command Editor

--- src/main/java/de/wk/betacore/customcommand/cmdParser.java
+++ src/main/java/de/wk/betacore/customcommand/cmdParser.java
@@ -1,9 +1,5 @@
 package de.wk.betacore.customcommand;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-
 public class cmdParser {
-    Player p = Bukkit.getPlayer("frfrfr");
-    p.
+
 }
